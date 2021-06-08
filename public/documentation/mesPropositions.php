50b76960592dc77412bffa21cd33904e2af9546b
Jeff Jordan <Jeff Jordan@Laptop-Jeff>
2021-06-08 12:10:51 +0200

    Correction erreurs de syntaxe

--- public/documentation/mesPropositions.php
+++ public/documentation/mesPropositions.php
@@ -16,7 +16,7 @@ require_once ($root . '/outil/bibliotheque.php');
    panel_head("Le point de vue global sur notre projet :");
   
   panel("Le cycle de vie de notre projet : ", 
-          . "<ul>"
+           "<ul>"
           . "<li>le cadrage : la définition des besoins et des objectifs de notre site</li>"
           . "<li>la conception : choix des outils , repartition des taches , planning de travail </li>"
         . "<li>conception : avec l'utilisation d'un outil qui nous a facilité le travail (<b>Le modèle MVC</b>) </li>"
@@ -29,14 +29,14 @@ require_once ($root . '/outil/bibliotheque.php');
          . "<li> la mise à jour des informations sur un stock de vaccin</li>"
           . "</ul>");
    panel("Notre avis :",
-         ."Nous avons personnelement trouvé ce sujet très interéssant parceque:" 
+         "Nous avons personnelement trouvé ce sujet très interéssant parceque:" 
          . "<p> premièrement , il parle d'un phénomène que nous vivons actuellement dans le monde : celui de la vaccination du COVID-19 , donc c'est un sujet qui touche à l'acutalité </p>"
          . "<p> Deuxièmement , ce sujet nous permet d'avoir une idée de comment ça se passe dans les centres de vaccination ( comment se passe la geston des vaccins , la gestion des patient , des stocks de vaccin...) , ce qui est très bénéfique pour notre culture personnelle.</p>"
          ."<p>Et troisièmement nous avons réelement apprécié la manière dont c'est déroulé l'UE car au vue des multiples DMs que nous avons  eu à traiter tout au long du semestre , cela a été très simple pour nous de réaliser ce projet qui était juste le condensé de tout les devoirs déjà remis</p>");
            "<br>
            <br>"
            
-           "<h5><b><i>Nous vous remercions ;) </h5></b></i>" 
+           ."<h5><b><i>Nous vous remercions ;) </h5></b></i>" 
    ?>
   </div>   
   
